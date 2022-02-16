0056528dd3c18c33c72e820509cc7e53fc8aeaef
chris <chri402s@stud.kea.dk>
2022-02-16 12:16:16 +0100

    Initial commit

---
+++ src/main/java/com/example/binarycalculator/BinaryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.binarycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/binarycalculator/BinaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binarycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
